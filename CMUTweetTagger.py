4d749819a383de5bcb0ee64850f56faf66ed9791
Italo Lopes Oliveira <italo.lopes.oliveira@gmail.com>
2018-10-10 20:01:09 +0200

    Updated the code for Python 3.x (only tested in Python 3.6)

--- CMUTweetTagger.py
+++ CMUTweetTagger.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/python
 # -*- coding: utf-8 -*-
 """Simple Python wrapper for runTagger.sh script for CMU's Tweet Tokeniser and Part of Speech tagger: http://www.ark.cs.cmu.edu/TweetNLP/
 
@@ -15,6 +15,7 @@ import shlex
 # useful (but not much).
 
 # NOTE this command is directly lifted from runTagger.sh
+# NOTE update this command to match with the .jar file in your project
 RUN_TAGGER_CMD = "java -XX:ParallelGCThreads=2 -Xmx500m -jar ark-tweet-nlp-0.3.2.jar"
 
 
@@ -27,13 +28,12 @@ def _split_results(rows):
                 parts = line.split('\t')
                 tokens = parts[0]
                 tags = parts[1]
-                confidence = float(parts[2])
+                confidence = float(parts[2].replace(",", "."))
                 yield tokens, tags, confidence
 
 
 def _call_runtagger(tweets, run_tagger_cmd=RUN_TAGGER_CMD):
     """Call runTagger.sh using a named input file"""
-
     # remove carriage returns as they are tweet separators for the stdin
     # interface
     tweets_cleaned = [tw.replace('\n', ' ') for tw in tweets]
@@ -55,7 +55,7 @@ def _call_runtagger(tweets, run_tagger_cmd=RUN_TAGGER_CMD):
     # ('hello\t!\t0.9858\nthere\tR\t0.4168\n\n',
     # 'Listening on stdin for input.  (-h for help)\nDetected text input format\nTokenized and tagged 1 tweets (2 tokens) in 7.5 seconds: 0.1 tweets/sec, 0.3 tokens/sec\n')
 
-    pos_result = result[0].strip('\n\n')  # get first line, remove final double carriage return
+    pos_result = result[0].decode().strip('\n\n')  # get first line, remove final double carriage return
     pos_result = pos_result.split('\n\n')  # split messages by double carriage returns
     pos_results = [pr.split('\n') for pr in pos_result]  # split parts of message by each carriage return
     return pos_results
@@ -77,23 +77,18 @@ def check_script_is_present(run_tagger_cmd=RUN_TAGGER_CMD):
         args = shlex.split(run_tagger_cmd)
         args.append("--help")
         po = subprocess.Popen(args, stdout=subprocess.PIPE)
-        # old call - made a direct call to runTagger.sh (not Windows friendly)
-        #po = subprocess.Popen([run_tagger_cmd, '--help'], stdout=subprocess.PIPE)
-        while not po.poll():
-            lines = [l for l in po.stdout]
-        # we expected the first line of --help to look like the following:
-        assert "RunTagger [options]" in lines[0]
+        assert("RunTagger [options]" in po.stdout.read().decode())
         success = True
     except OSError as err:
-        print "Caught an OSError, have you specified the correct path to runTagger.sh? We are using \"%s\". Exception: %r" % (run_tagger_cmd, repr(err))
+        print("Caught an OSError, have you specified the correct path to runTagger.sh? We are using \"%s\". Exception: %r" % (run_tagger_cmd, repr(err)))
     return success
 
 
 if __name__ == "__main__":
-    print "Checking that we can see \"%s\", this will crash if we can't" % (RUN_TAGGER_CMD)
+    print("Checking that we can see \"%s\", this will crash if we can't" % (RUN_TAGGER_CMD))
     success = check_script_is_present()
     if success:
-        print "Success."
-        print "Now pass in two messages, get a list of tuples back:"
+        print("Success.")
+        print("Now pass in two messages, get a list of tuples back:")
         tweets = ['this is a message', 'and a second message']
-        print runtagger_parse(tweets)
+        print(runtagger_parse(tweets))
